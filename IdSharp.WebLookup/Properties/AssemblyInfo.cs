f76a471279189d7e87f05a5bfff35c67650ff03d
Jud White <jwhite@cdtag.com>
2011-05-06 16:49:59 -0500

    Change IdSharp.WebLookup to AnyCPU

--- IdSharp.WebLookup/Properties/AssemblyInfo.cs
+++ IdSharp.WebLookup/Properties/AssemblyInfo.cs
@@ -1,22 +1,4 @@
-﻿/*
- *   IdSharp - A tagging library for .NET
- *   Copyright (C) 2007  Jud White
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License along
- *   with this program; if not, write to the Free Software Foundation, Inc.,
- *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -27,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jud White")]
 [assembly: AssemblyProduct("IdSharp.WebLookup")]
-[assembly: AssemblyCopyright("Copyright © 2007 Jud White")]
+[assembly: AssemblyCopyright("Copyright © 2007-2011 Jud White")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -48,5 +30,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.1.*")]
